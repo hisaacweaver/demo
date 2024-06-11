df6ce3d3acded2ad0d530203e669aed438f42818
Isaac Weaver <isaac.weaver99@gmail.com>
2024-06-11 10:00:13 -0600

    initial commit

---
+++ demo.py
@@ -0,0 +1,16 @@
+def demo_function_addition(x, y):
+    """
+    Takes two numbers and returns their sum.
+
+    Args:
+        x (int or float): The first number.
+        y (int or float): The second number.
+
+    Returns:
+        int or float: The sum of the two numbers.
+    """
+    return x + y
+
+
+result = demo_function_addition(12, 14)
+print(result)
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Git Demo in VSCode
+
+This is a demo showing some of the git features integrated into VSCode
